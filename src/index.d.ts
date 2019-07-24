46196b76ad4d5fb4707f4b5264a7b339daf331a7
Horus Lugo <horusgoul@gmail.com>
2019-07-24 12:29:54 +0200

    Fixes PNG and JPG support

--- src/index.d.ts
+++ src/index.d.ts
@@ -2,3 +2,13 @@ declare module '*.svg' {
   const content: React.ComponentType<React.SVGAttributes<SVGElement>>;
   export default content;
 }
+
+declare module '*.png' {
+  const content: string;
+  export default content;
+}
+
+declare module '*.jpg' {
+  const content: string;
+  export default content;
+}
